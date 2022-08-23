9803a52a8fe978f9dd1581c5f6d2ee829beb5b55
Anishgamer777 <shobhashama9@gmail.com>
2022-08-23 18:24:48 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,23 +1,33 @@
 module.exports = {
   owners: ["707066042563821569"],
   nodes: [{ 
-    identifier: 'NODE-MAIN-2', 
-    host: 'n1.lavalink.milrato.com'
-  , password: 'discord.gg/milrato',
-    port:10150 ,
-    secure: true, },
-         { 
-identifier: 'NODE-MAIN-1', 
-host: 'n1.lavalink.milrato.com', password: 'discord.gg/milrato', 
-port:10050 , 
+           "identifier": 'NODE-MAIN-1',
+            "host": "lava1.cruzstudio.tech",
+                "password": "cruzstudio.tech",
+                "port": 80
+         },
+            {identifier": 'NODE-MAIN-2',
+            "host": "lava2.cruzstudio.tech",
+                "password": "cruzstudio.tech",
+                
 secure: true,
- },
-   { 
-identifier: 'NODE-MAIN-3', 
-host: 'n1.lavalink.milrato.com', password: 'discord.gg/milrato', 
-port:10450 , 
+"port": 80
+        },
+        {identifier": 'NODE-MAIN-3',
+            "host": "lava3.cruzstudio.tech",
+             secure: true,
+   "password": "cruzstudio.tech",
+                "port": 80
+         },
+            {
+identifier": 'NODE-MAIN-4',
 secure: true,
- }      ],
+            "host": "lava4.cruzstudio.tech",
+                "password": "cruzstudio.tech",
+                "port": 80
+            }],
   prefix: '?',
   updateSlash: true,
 };
+
+   
